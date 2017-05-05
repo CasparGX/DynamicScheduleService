c0f9270da640555275514367089cd3f826e37253
CasparGX <630248976@qq.com>
2017-05-05 17:49:44 +0800

    Update

---
+++ src/main/java/ssm/dao/ScheduleMapper.java
@@ -0,0 +1,32 @@
+package ssm.dao;
+
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+import ssm.model.Schedule;
+import ssm.model.ScheduleExample;
+
+public interface ScheduleMapper {
+    int countByExample(ScheduleExample example);
+
+    int deleteByExample(ScheduleExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Schedule record);
+
+    int insertSelective(Schedule record);
+
+    List<Schedule> selectByExample(ScheduleExample example);
+
+    List<Schedule> selectByExampleGroupByShareCode(ScheduleExample example);
+
+    Schedule selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") Schedule record, @Param("example") ScheduleExample example);
+
+    int updateByExample(@Param("record") Schedule record, @Param("example") ScheduleExample example);
+
+    int updateByPrimaryKeySelective(Schedule record);
+
+    int updateByPrimaryKey(Schedule record);
+}
---
+++ src/main/java/ssm/model/Schedule.java
@@ -0,0 +1,53 @@
+package ssm.model;
+
+public class Schedule {
+    private Integer id;
+
+    private Integer uid;
+
+    private Integer cid;
+
+    private String shareCode;
+
+    private String city;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getUid() {
+        return uid;
+    }
+
+    public void setUid(Integer uid) {
+        this.uid = uid;
+    }
+
+    public Integer getCid() {
+        return cid;
+    }
+
+    public void setCid(Integer cid) {
+        this.cid = cid;
+    }
+
+    public String getShareCode() {
+        return shareCode;
+    }
+
+    public void setShareCode(String shareCode) {
+        this.shareCode = shareCode == null ? null : shareCode.trim();
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city == null ? null : city.trim();
+    }
+}
